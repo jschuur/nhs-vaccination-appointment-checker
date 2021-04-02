a0b04b57243ab72cc31e8b220cd038fcf5efd5a7
Joost Schuur <jschuur@jschuur.com>
2021-04-02 14:55:07 +0100

    Show update frequency at launch

--- src/index.js
+++ src/index.js
@@ -9,14 +9,14 @@ const {
 } = require('./lib.js');
 
 // Check every 120 minutes
-const CHECK_FREQUENCY = 120 * 60000;
+const CHECK_FREQUENCY = 120;
 
 (async () => {
   // For testing purposes, check for a change immediately, comparing the current text to an empty string
   if (process.env.NODE_ENV === 'dev') checkForChange();
   else setCurrentEligibility(await getCurrentEligibility());
 
-  setInterval(checkForChange, CHECK_FREQUENCY);
+  setInterval(checkForChange, CHECK_FREQUENCY * 60000);
 
-  log(`Monitoring ${NHS_URL} for eligibility changes...`);
+  log(`Monitoring ${NHS_URL} for eligibility changes every ${CHECK_FREQUENCY} minutes ...`);
 })();
